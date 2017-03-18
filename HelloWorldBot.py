767bd5203ef3c744e4086354c8492439d0eb0da6
hazem osama ibrahim <hazem3500@gmail.com>
2017-03-18 21:23:51 +0200

    Update HelloWorldBot.py

--- HelloWorldBot.py
+++ HelloWorldBot.py
@@ -1,14 +1,11 @@
 from flask import Flask, request
-from wit import Wit
 import requests
 import json
 import traceback
 import random
-
 app = Flask(__name__)
 
-client = Wit(access_token="2K7GXJSRTF7B5ZB74PNZBZQPGFYIIRKC")
-token = "EAAD0FIbuQqcBAJaKgjIVEeQqRjyyb0iSoy7ay4SAo1YDIJxxma8OVqnYLdMtZCY5aSKhkkGYJefIFlHgALKLAJvC2TaBWEdnLS113cpvttENvQOZAaOvbygurhcflATMYamDnoQlC8DocpFcXqnXeAbfQsHAuGmZAqIr6PY8wZDZD"
+token = "EAAD0FIbuQqcBAAZBr6ZAvNEcLwCIjF6mQ0QDst9pZCV3T1UohpBCiP1SH4e6nE7bHXPTttLGPzZBzfpWSrt3umJ4dp89ZBOVN9MZCCNZCEZAkhKXt5M95lTvHCEuW6sCsdmNr3ZArOu7JRXxWYO2x0MYTvplaW4Lfkv15CtqoOyPc5QZDZD"
 
 @app.route('/webhook', methods=['GET', 'POST'])
 def webhook():
@@ -17,8 +14,7 @@ def webhook():
       data = json.loads(request.data)
       text = data['entry'][0]['messaging'][0]['message']['text'] # Incoming Message Text
       sender = data['entry'][0]['messaging'][0]['sender']['id'] # Sender ID
-      resp = client.converse(sender, text, {})
-      payload = {'recipient': {'id': sender}, 'message': {'text': "m3lsh"}} # We're going to send this back
+      payload = {'recipient': {'id': sender}, 'message': {'text': "Hello World"}} # We're going to send this back
       r = requests.post('https://graph.facebook.com/v2.6/me/messages/?access_token=' + token, json=payload) # Lets send it
     except Exception as e:
       print traceback.format_exc() # something went wrong
@@ -26,6 +22,7 @@ def webhook():
     if request.args.get('hub.verify_token') == 'banana':
       return request.args.get('hub.challenge')
     return "Wrong Verify Token"
+  return "Hello World" #Not Really Necessary
 
 if __name__ == '__main__':
   app.run(debug=True)
