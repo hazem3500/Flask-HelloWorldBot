a3daf0fb818516bb331d047d7d286c529d282b5a
hazem osama ibrahim <hazem3500@gmail.com>
2017-03-18 19:56:13 +0200

    Update HelloWorldBot.py

--- HelloWorldBot.py
+++ HelloWorldBot.py
@@ -1,82 +1,28 @@
-from __future__ import print_function
-
-import os
-import sys
-import json
 from flask import Flask, request
 import requests
-
-
-from wit import Wit
-
-
-CLIENT_ACCESS_TOKEN = '7S2D2JZ4EGCIGC6GUXATTOYFVBSVQ65F'
-ACCESS_TOKEN = "EAAD0FIbuQqcBAEDdtxtURYnmZArL4Xu1GaokZBZBk6UTM6vqufcYGEvd5q1OfZAicWBScBzMcUfakmsZCcFb71imQSP3nrFDezqgdyZASOmH9JA020ZBCKcbpikLjQGMkkewEWf5cHmoeoiioZC8EAEtuepQRwjeMqinMWX3sWT7CgZDZD"
-
-
-
-
-
-
-
+import json
+import traceback
+import random
 app = Flask(__name__)
 
+token = "<ACCESS_TOKEN_HERE>"
 
-#
-#def send(request, response):
-#    """
-#    Sender function
-#    """
-#    # We use the fb_id as equal to session_id
-#    fb_id = request['session_id']
-#    text = response['text']
-#    # send message
-#    reply(fb_id, text)
-#
-
-def reply(user_id, msg):
-    data = {
-        "recipient": {"id": user_id},
-        "message": {"text": msg}
-    }
-    resp = requests.post("https://graph.facebook.com/v2.6/me/messages?access_token=" + ACCESS_TOKEN, json=data)
-    return resp.content
-
-
-@app.route('/', methods=['GET'])
-def handle_verification():
-    return request.args['hub.challenge']
-
-
-@app.route('/', methods=['POST'])
-def handle_incoming_messages():
-	data = request.json
-	if data['object'] == "page":
-		for entry in data['entry']:
-			pageID = entry['id']
-			for msg in entry['messaging']:
-				if msg['message']:
-					sender = msg['sender']['id']
-					message = msg['message']['text']
-
-
-					context0 = {}
-					response = client.converse(sender, message, context0)
-
-
-					reply(sender, str(response['msg']))
-					return "ok"
-				else:
-					return "ok"
-	return "ok"
-#
-#
-#actions = {
-#    'send': send,
-#}
-
-
-client = Wit(access_token=CLIENT_ACCESS_TOKEN)
+@app.route('/webhook', methods=['GET', 'POST'])
+def webhook():
+  if request.method == 'POST':
+    try:
+      data = json.loads(request.data)
+      text = data['entry'][0]['messaging'][0]['message']['text'] # Incoming Message Text
+      sender = data['entry'][0]['messaging'][0]['sender']['id'] # Sender ID
+      payload = {'recipient': {'id': sender}, 'message': {'text': "Hello World"}} # We're going to send this back
+      r = requests.post('https://graph.facebook.com/v2.6/me/messages/?access_token=' + token, json=payload) # Lets send it
+    except Exception as e:
+      print traceback.format_exc() # something went wrong
+  elif request.method == 'GET': # For the initial verification
+    if request.args.get('hub.verify_token') == 'banana':
+      return request.args.get('hub.challenge')
+    return "Wrong Verify Token"
+  return "Hello World" #Not Really Necessary
 
 if __name__ == '__main__':
-    app.run(debug=True)
+  app.run(debug=True)
